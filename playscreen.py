1c2116b0f0da51c82f0c2451a719161c12462df7
Vaughn-Mina <126901084+Vaughn-Mina@users.noreply.github.com>
2023-05-02 16:20:42 -0700

    just to commit and push

--- playscreen.py
+++ playscreen.py
@@ -4,7 +4,7 @@ Goals
 -create a sprite and sprite sheet for 1 character
     -2 different sprite animation for idle
     -8 different sprite animation for walk 
-    -to duplicate image 
+    -to duplicate image select the right tool
 
 
 
